b9404808b52375d7e4cb823b0e2d317797c800bc
liuyu <12345678>
2019-08-16 18:00:50 +0800

    更新

--- src/main/java/com/member/service/config/DataSourceConfig.java
+++ src/main/java/com/member/service/config/DataSourceConfig.java
@@ -3,7 +3,6 @@ package com.member.service.config;
 import com.alibaba.druid.pool.DruidDataSource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
